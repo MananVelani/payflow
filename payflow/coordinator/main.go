beaa2703af1a804c98ed288c473c2bc21fcce2c1
chatterjee-sid <sid.c.ver02@gmail.com>
2026-03-22 02:06:03 +0530

    int ~/coordinator + restructure ~/proto (1)

--- payflow/coordinator/main.go
+++ payflow/coordinator/main.go
@@ -13,6 +13,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
 )
 
 // CoordinatorNode implements all 3 required server interfaces
@@ -107,6 +108,51 @@ func (c *CoordinatorNode) RegisterWorker(ctx context.Context, req *workerPb.Regi
 
 // Note: You will eventually need to implement Heartbeat, PollTasks, and ReportResult here too!
 
+// Heartbeat receives periodic health checks from workers 
+func (c *CoordinatorNode) Heartbeat(ctx context.Context, req *workerPb.HeartbeatRequest) (*workerPb.HeartbeatResponse, error) {
+	// In Week 2, we will use this to reset a timeout timer. 
+	// For now, just log it.
+	log.Printf("[LEADER %s] Received heartbeat from worker %s. Load: %d", c.ID, req.WorkerId, req.CurrentLoad)
+	
+	return &workerPb.HeartbeatResponse{
+		Acknowledged: true,
+	}, nil
+}
+
+// ReportResult handles the outcome of a payment from a worker 
+func (c *CoordinatorNode) ReportResult(ctx context.Context, req *workerPb.TaskResult) (*workerPb.ResultAck, error) {
+	// In Week 2, we will forward this result to C4 (Payment Log Service).
+	log.Printf("[LEADER %s] Received result for task %s from worker %s", c.ID, req.TaskId, req.WorkerId)
+	
+	return &workerPb.ResultAck{
+		Acknowledged: true,
+	}, nil
+}
+
+// PollTasks is a SERVER STREAMING endpoint where the coordinator pushes tasks to the worker 
+// Notice there is no return object, only an error return.
+func (c *CoordinatorNode) PollTasks(req *workerPb.PollRequest, stream workerPb.WorkerManagement_PollTasksServer) error {
+	log.Printf("[LEADER %s] Worker %s started polling for tasks", c.ID, req.WorkerId)
+	
+	// For Week 1 Integration, we just send one dummy task and close the stream
+	dummyTask := &workerPb.TaskAssignment{
+		Epoch:          int64(c.Epoch),
+		TaskId:         "dummy-txn-12345",
+		IdempotencyKey: "idem-999",
+		Amount:         150.00,
+	}
+	
+	// stream.Send() pushes the data down the open TCP connection
+	if err := stream.Send(dummyTask); err != nil {
+		log.Printf("Failed to send task to worker %s: %v", req.WorkerId, err)
+		return err
+	}
+	
+	// Returning nil tells gRPC "I am done streaming, you can close the connection"
+	// In Week 2, this will be an infinite loop listening to a Go channel!
+	return nil 
+}
+
 func main() {
 	node := &CoordinatorNode{
 		ID:      "coordinator-1",
@@ -127,6 +173,8 @@ func main() {
 	coordPb.RegisterCoordinatorClusterServer(grpcServer, node)
 	paymentPb.RegisterPaymentGatewayServer(grpcServer, node)
 	workerPb.RegisterWorkerManagementServer(grpcServer, node)
+
+	reflection.Register(grpcServer)
 
 	log.Printf("Coordinator %s starting in %s state on port %s...", node.ID, node.State, port)
 
